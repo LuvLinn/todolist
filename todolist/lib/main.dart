edf983e268eec8bd6ff85561518b4f05de067f45
LuvLinn <96067781+LuvLinn@users.noreply.github.com>
2023-04-09 20:56:09 +0700

    updated

---
+++ todolist/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/modal/items.dart';
+import 'package:todolist/widget/card_modal_bottom.dart';
+import 'widget/card_body_widget.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<DataItems> items = [];
+
+  void _handleAddTask(String name) {
+    final newItem = DataItems(id: DateTime.now().toString(), name: name);
+    setState(() {
+      items.add(newItem);
+    });
+  }
+
+  void _handleDeleteTask(String id) {
+    setState(() {
+      items.removeWhere((item) => item.id == id);
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'ToDoList',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 31,
+          vertical: 30,
+        ),
+        child: Column(
+          children: items
+              .map((item) => CardBody(
+                    index: items.indexOf(item),
+                    item: item,
+                    handleDelete: _handleDeleteTask,
+                  ))
+              .toList(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+            ),
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) {
+              return ModalBottom(addTask: _handleAddTask);
+            },
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          size: 40,
+        ),
+      ),
+    );
+  }
+}
